b2baee7ace5816538784fa99d2f1f8fc54da55e4
pavlik111188 <ps@binotel.com>
2018-03-20 22:24:22 +0200

    started working on API and added example of modal for Dimas

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalDirective } from '../../typescripts/free';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  @ViewChild('autoShownModal') public autoShownModal:ModalDirective;
+  public isModalShown: boolean = true;
+
+  public showModal():void {
+    this.isModalShown = true;
+  }
+
+  public hideModal():void {
+    this.autoShownModal.hide();
+  }
+
+  public onHidden():void {
+    this.isModalShown = false;
+  }
 
   constructor() { }
 
