afd856dbb780dbe2bdd039df65755931b1d21bc1
postoliuk <kitobones@gmail.com>
2018-04-05 23:43:48 +0300

    dashboard user component - added edit profile;

--- src/app/dashboards/dashboard-user/dashboard-user.component.ts
+++ src/app/dashboards/dashboard-user/dashboard-user.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import {User} from "../../shared/models/user.model";
+import {AuthService} from "../../shared/services/auth.service";
+import {ToastComponent} from "../../shared/toats/toast.component";
+import {UserService} from "../../shared/services/user.service";
+
+
 @Component({
   selector: 'app-dashboard-user',
   templateUrl: './dashboard-user.component.html',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardUserComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  isLoading = true;
+
+  constructor(private auth: AuthService,
+              public toast: ToastComponent,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.getUser();
+  }
+
+  getUser() {
+    this.userService.getUser(this.auth.currentUser).subscribe(
+        data => this.user = data,
+        error => console.log(error),
+        () => this.isLoading = false
+    );
+  }
+
+  save(user: User) {
+    this.userService.editUser(user).subscribe(
+        res => this.toast.setMessage('account settings saved!', 'success'),
+        error => console.log(error)
+    );
   }
 
 }
