dc9fd9f49ddc90eecd3cd79cc5abd13a417ea248
postoliuk <kitobones@gmail.com>
2018-04-25 21:43:53 +0300

    ability to edit user profile;

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -57,6 +57,7 @@ export class AuthService {
     setCurrentUser(decodedUser) {
         this.loggedIn = true;
         this.currentUser._id = decodedUser._id;
+        this.currentUser.email = decodedUser.email;
         this.currentUser.role = decodedUser.role;
         decodedUser.role === 'admin' ? this.isAdmin = true : this.isAdmin = false;
         delete decodedUser.role;
