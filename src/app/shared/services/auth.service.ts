966bff8d4bd76686454536989f690e59e6549fa4
postoliuk <kitobones@gmail.com>
2018-04-25 22:47:58 +0300

    show user name in header when logge in; when it's empty - email;

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -57,6 +57,7 @@ export class AuthService {
     setCurrentUser(decodedUser) {
         this.loggedIn = true;
         this.currentUser._id = decodedUser._id;
+        this.currentUser.first_name = decodedUser.first_name;
         this.currentUser.email = decodedUser.email;
         this.currentUser.role = decodedUser.role;
         decodedUser.role === 'admin' ? this.isAdmin = true : this.isAdmin = false;
