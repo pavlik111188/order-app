d97bb40ad8f3ec9904e9738135e08e2f7463f74f
postoliuk <kitobones@gmail.com>
2018-05-01 22:34:39 +0300

    events for users and guests;

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {EventService} from "../../shared/services/event.service";
+import { Event } from '../../shared/models/event.model';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {}
+  events: Event[] = [];
+  isLoading = true;
+
+  constructor(private eventService: EventService) {}
 
   ngOnInit() {
+    this.getEvents();
+  }
 
+  getEvents() {
+    this.eventService.getEvents().subscribe(
+        data => this.events = data,
+        error => console.log(error),
+        () => this.isLoading = false
+    );
   }
 }
--- src/app/dashboards/dashboard-user/dashboard-user.component.ts
+++ src/app/dashboards/dashboard-user/dashboard-user.component.ts
@@ -4,6 +4,8 @@ import {User} from "../../shared/models/user.model";
 import {AuthService} from "../../shared/services/auth.service";
 import {ToastComponent} from "../../shared/toats/toast.component";
 import {UserService} from "../../shared/services/user.service";
+import {EventService} from "../../shared/services/event.service";
+import { Event } from '../../shared/models/event.model';
 
 
 @Component({
@@ -14,19 +16,30 @@ import {UserService} from "../../shared/services/user.service";
 export class DashboardUserComponent implements OnInit {
 
   user: User;
+  events: Event[] = [];
   isLoading = true;
 
   constructor(private auth: AuthService,
+              private eventService: EventService,
               public toast: ToastComponent,
               private userService: UserService) { }
 
   ngOnInit() {
     this.getUser();
+    this.getEvents();
   }
 
   getUser() {
     this.userService.getUser(this.auth.currentUser).subscribe(
         data => this.user = data,
+        error => console.log(error),
+        () => this.isLoading = false
+    );
+  }
+
+  getEvents() {
+    this.eventService.getEvents().subscribe(
+        data => this.events = data,
         error => console.log(error),
         () => this.isLoading = false
     );
